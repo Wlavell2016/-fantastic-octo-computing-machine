6360e9775d9537e2c43dad7037f1accd10a875e4
Wlavell2016 <william.lavell@gmail.com>
2017-04-17 19:05:28 +0700

    making changes

---
+++ examples/backup.js
@@ -0,0 +1,24 @@
+// rez.forEach(function(location) {
+//   console.log((location[0]),(location[1]),(location[2]));
+//   console.log((location[1]));
+//   console.log((location[2]));
+//
+// });
+
+// var rez = [
+//   {
+//     title: 'batchewana',
+//     lat: 46.528494,
+//     lng: -84.261409
+//   },
+//   {
+//     title: 'caldwell',
+//     lat: 42.052845,
+//     lng: -82.597453
+//   },
+//   {
+//     title: 'delaware',
+//     lat: 42.581660,
+//     lng: -81.893242
+//   }
+// ];
